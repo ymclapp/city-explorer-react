352fb7fc50e01739a12978b19e17c0ed03cf772e
ymcla <ymclapp@yahoo.com>
2021-09-06 15:46:46 -0500

    updated main for search label, input form, and button

--- src/components/main.js
+++ src/components/main.js
@@ -1,11 +1,27 @@
 import React from "react";
 
 class Main extends React.Component {
+    handleLocationSearch = submitEvent => {
+        submitEvent.preventDefault();
+
+        console.log('submitted', submitEvent.target);
+    }
+    
+    
     render () {
         return (
-            <Main>
-                Hi!!!!!
-            </Main>
+            <main>
+                <form onSubmit = {this.handleLocationSearch}>
+                    <label>
+                        Search for a location:
+                        {' '} {/*adds a space between the label and the input box*/}
+                        <input type = "text" name = "search" placeholder = "location" />
+                    </label>
+                    <div>
+                        <button type = "submit">Search</button>
+                    </div>
+                </form>
+            </main>
         )
     }
 }

25f16e5a5a11826925574c097c6a859fdeb541a6
ymcla <ymclapp@yahoo.com>
2021-09-07 17:31:37 -0500

    addeed the code from today into the appTues.js file

--- src/components/appTues.js
+++ src/components/appTues.js
@@ -1,25 +1,66 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
 
 class App extends React.Component {
-    state = {  //does the same as the constructor(props) super (props) etc
+    //does the same as the constructor(props) super (props) etc
+    state = {  
         q: null,
+        location:  null,
     };
     
-    handleSearch = event => {
+    handleSearch = async event => {  //have to have async here to have await to work below
         event.preventDefault();
 
         let form = event.target;
         let input = form.elements.search;
         let q = input.value;
         console.log(q);
-        this.setState({q});
+        
+        //assign q in state to be value of q
+        this.setState({q, location: null});  //clears the location info while searching for a new one
+
+        const url = `https://us1.locationiq.com/v1/reverse.php`;
+        const response = await axios.get(url, {
+            params: {
+                key: process.env.REACT_APP_LOCATION_KEY,  //needs to match .env and it has to start with REACT_APP_ then the name you want it to have
+                q,
+                format: 'json',
+            }
+        });
+           
+        console.log(response);
+        
+        const location = response.data[0];
+        this.setState({location});
     };
     
     render () {
         return (
-            <div className = "App"
-        )
+            <div className = "App">
+                <form onSubmit = {this.handleSearch}>
+                    <label>
+                        Search for a location:
+                        {' '}
+                        <input type = "text" name = "search" placeholder = "Location" />
+                    </label>
+                    <div>
+                        <button type = "submit">Search</button>
+                    </div>
+                </form>
 
+                {this.state.q &&
+                <>
+                <h2>Search: {this.state.q}</h2>
+                {this.state.location ?
+                <p>Display Name:  {this.state.location.display_name}</p>
+                : <p>Loading...</p>
+                }
+                </>
+                }
+            </div>
+        );
     }
 }
+
+export default App;
